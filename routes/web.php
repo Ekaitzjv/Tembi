1800ab562a0721850986388ae5bced71b8c48e5f
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-08-27 19:51:45 +0200

    Testing the ORM
    Consulting to the DDBB much easier than MVC

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,30 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Image;
 
 Route::get('/', function () {
+
+    //para sacar todas las imagenes de la db
+    $images = Image::all();
+    foreach($images as $image){
+        echo $image->image_path."<br/>";
+        echo $image->description."<br/>";
+        echo $image->user->name.' '.$image->user->surname;
+
+        //que muestre comentarios si los hay
+        if(count($image->comments) >= 1 ){
+            echo '<h4>Comentarios:</h4>';
+            foreach($image->comments as $comment){
+                echo $comment->user->name.' '.$comment->user->surname.': ';
+                echo $comment->content.'<br/>';
+            }
+        }
+        //sacar los likes
+        echo 'Likes: '.count($image->likes);
+        echo "<hr/>";
+    }
+
+    die();
     return view('welcome');
 });
