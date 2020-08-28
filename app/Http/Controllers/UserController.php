32a3bdff3b8b3dcdd6331fe0c71f16289d02ad2c
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-08-28 18:08:01 +0200

    receive form data
    receive form data and POST/GET error solved

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,16 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
+        $id =\Auth::user()->id;
+        $email =\Auth::user()->email;
+
+        $validate = $this->validate($request, [  
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            //si el nombre de usuario coincide con el id entonces solo se hará la comprobación
+            'username' => 'required|string|max:255|unique:users,surname,'.$id,
+            'email' => 'required|string|max:255|email|unique:users,email,'.$id,
+        ]);
 
         $id = \Auth::user()->id;
         $name = $request->input('name');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Red_Social') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Red Social') }}
+                    {{ config('app.name', 'Red_Social') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -47,22 +47,19 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-                        
                         @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
                         @else
-
                         <li class="nav-item">
-                            <a  class="nav-link" href="">Main</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a  class="nav-link" href="">Upload Image</a>
+                            <a class="nav-link" href="">Main</a>
                         </li>
 
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Upload Image</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -77,8 +74,8 @@
                                 <a class="dropdown-item" href="{{ route('edit') }}">
                                     Edit profile
                                 </a>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
