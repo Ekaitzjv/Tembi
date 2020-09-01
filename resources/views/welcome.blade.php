1b39b3da3fd70a78877c30be72fb41cb8926ab40
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-09-01 12:23:04 +0200

    Name changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Red_Social
                 </div>
 
                 <div class="links">
