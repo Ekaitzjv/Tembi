a6d1d55dbe8eba07c79ba81798250fd5255ce0fa
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-09-01 21:55:32 +0200

    Upload user profile image
    UserController.php: give new values, upload image and save.
    filesystems.php: created 2 new folders to save images.
     edit.blade.php: alert, new field to upload files(images) and their conditionals.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class UserController extends Controller
 {
@@ -11,9 +13,12 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
-        $id =\Auth::user()->id;
-        $email =\Auth::user()->email;
 
+        //conseguir el usuario identificado
+        $user = \Auth::user();
+        $id = $user->id;
+
+        //validación del formulario
         $validate = $this->validate($request, [  
             'name' => 'required|string|max:255',
             'surname' => 'required|string|max:255',
@@ -21,12 +26,36 @@ class UserController extends Controller
             'username' => 'required|string|max:255|unique:users,surname,'.$id,
             'email' => 'required|string|max:255|email|unique:users,email,'.$id,
         ]);
-
+        
+        //Recoger datos del formulario
         $id = \Auth::user()->id;
         $name = $request->input('name');
         $surname = $request->input('surname');
         $username = $request->input('username');
         $email = $request->input('email');
+        
+        //asignar nuevos valores al objeto del usuario
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->username = $username;
+        $user->email = $email;
+
+        //subir la imagen
+        $image_path = $request->file('image_path');
+        if($image_path){
+            //Poner nombre único
+            $image_path_name = time().$image_path->getClientOriginalName();
+            //Guardarla en la carpeta storage (storage/app/users)
+            Storage::disk('users')->put($image_path_name, File::get($image_path));
+            //setear el nombre de la imagen en el objeto
+            $user->image = $image_path_name;
+        }
+
+        //ejecutar consulta y cambios en la ddbb
+        $user->update();
+
+        return redirect()->route('edit')
+                         ->with(['message'=>'User updated successfully']);
 
 
     }
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -4,12 +4,33 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
+            @if(session('message'))
+                <div class="alert alert-success">
+                {{ session('message')}}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">Edit profile</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user.update') }}" aria-label="Account settings">
+                    <form method="POST" action="{{ route('user.update') }}" enctype="multipart/form-data" aria-label="Account settings">
                         @csrf
+                        
+                        
+                        <div class="form-group row">
+                            <label for="image_path" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="image_path" type="file" class="form-control{{ $errors->has('image_path') ? 'is-invalid' : '' }}" name="image_path">
+
+                                @if($errors->has('image_path'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('image_path') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
@@ -54,7 +75,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
