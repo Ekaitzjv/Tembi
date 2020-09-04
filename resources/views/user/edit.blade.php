cfe3d53ee19360dc69d4140ecd0cfd3d3f15275b
Ekaitz Jimenez <ekaitzj@gmail.com>
2020-09-04 17:25:37 +0200

    Give design to profile image
    style.css: profile image design.
    adit.blade.php: image moved and added to other class.

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -6,31 +6,36 @@
         <div class="col-md-8">
 
             @if(session('message'))
-                <div class="alert alert-success">
+            <div class="alert alert-success">
                 {{ session('message')}}
-                </div>
+            </div>
             @endif
             <div class="card">
                 <div class="card-header">Edit profile</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user.update') }}" enctype="multipart/form-data" aria-label="Account settings">
+                    <form method="POST" action="{{ route('user.update') }}" enctype="multipart/form-data"
+                        aria-label="Account settings">
                         @csrf
-                        
                         
                         <div class="form-group row">
                             @if(Auth::user()->image)
-                                <img src="{{ route('user.image',['filename'=>Auth::user()->image]) }}"/>
+                            <img src="{{ route('user.image',['filename'=>Auth::user()->image]) }}" class="avatar" />
                             @endif
-                            <label for="image_path" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+                        </div>
 
+                        <div class="form-group row">
+                            <label for="image_path"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
                             <div class="col-md-6">
-                                <input id="image_path" type="file" class="form-control{{ $errors->has('image_path') ? 'is-invalid' : '' }}" name="image_path">
+                                <input id="image_path" type="file"
+                                    class="form-control{{ $errors->has('image_path') ? 'is-invalid' : '' }}"
+                                    name="image_path">
 
                                 @if($errors->has('image_path'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('image_path') }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('image_path') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -39,40 +44,47 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                    name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                            <label for="surname"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
 
                             <div class="col-md-6">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ Auth::user()->surname }}" required autocomplete="surname" autofocus>
+                                <input id="surname" type="text"
+                                    class="form-control @error('surname') is-invalid @enderror" name="surname"
+                                    value="{{ Auth::user()->surname }}" required autocomplete="surname" autofocus>
 
                                 @error('surname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-                        
+
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            <label for="username"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ Auth::user()->username }}" required autocomplete="name" autofocus>
+                                <input id="username" type="text"
+                                    class="form-control @error('username') is-invalid @enderror" name="username"
+                                    value="{{ Auth::user()->username }}" required autocomplete="name" autofocus>
 
                                 @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -81,12 +93,13 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
