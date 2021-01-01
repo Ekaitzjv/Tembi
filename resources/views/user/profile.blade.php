9bb823d431eb51f5b8e50219941200b7133fc8f0
Ekaitz Jimenez <ekaitzj@gmail.com>
2021-01-01 22:39:55 +0100

    Profile improved
    Profile image and data from user added.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,12 +4,28 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
+
+            <div class="data-username-profile">
+                <!--Imagen avatar-->
+                    @if($user->image)
+                    <div class="container-avatar avatar-profile">
+                        <a href=" {{$user->username}}">
+                            <img src="{{ route('user.image',['filename'=>$user->image]) }}" />
+                        </a>
+                    </div>
+                    @endif
+                <div class="data-user">
+                    <h2 class="username-profile">{{$user->username}}</h2>
+                    <h2 class="name-surname-profile">{{$user->name.' '.$user->surname}}</h2>
+                    <p class="description-profile">{{$user->description}}</p>
+                </div>
+            </div>
+
             <!--Bucle de publicaciones-->
             @foreach($user->images as $image)
-                @include('includes.image', ['image'=>$image])
+            @include('includes.image', ['image'=>$image])
             @endforeach
-            
+
         </div>
     </div>
 </div>
