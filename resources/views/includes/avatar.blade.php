0044db49937e35ce41db24ff35c5e1845ffca205
Ekaitz Jimenez <ekaitzj@gmail.com>
2021-01-02 22:49:25 +0100

    Default avatar added

--- resources/views/includes/avatar.blade.php
+++ resources/views/includes/avatar.blade.php
@@ -1,5 +1,9 @@
 @if(Auth::user()->image)
 <div class="container-avatar">
-    <img src="{{ route('user.image',['filename'=>Auth::user()->image]) }}" class="avatar" />
+    <img class="avatar" src="{{ route('user.image',['filename'=>Auth::user()->image]) }}" />
+</div>
+@else
+<div class="container-avatar">
+    <img class="avatar" src="{{ asset('img/default-avatar.jpg')}}" />
 </div>
 @endif
