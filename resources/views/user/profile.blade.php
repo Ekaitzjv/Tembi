54d70b985b7eca19d8ff9be627e5c6460cfe3be8
Ekaitz Jimenez <ekaitzj@gmail.com>
2021-01-02 22:48:13 +0100

    Design of the profile
    style.css: 3 dots design. User profile design.
    profile.blade.php: default avatar added.
    Profile improved. No posts warning.

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,27 +4,38 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-
-            <div class="data-username-profile">
+            <div class="data-user-profile">
                 <!--Imagen avatar-->
-                    @if($user->image)
-                    <div class="container-avatar avatar-profile">
-                        <a href=" {{$user->username}}">
-                            <img src="{{ route('user.image',['filename'=>$user->image]) }}" />
-                        </a>
-                    </div>
-                    @endif
+                @if($user->image)
+                <div class="avatar-profile">
+                    <img src="{{ route('user.image',['filename'=>$user->image]) }}" />
+                </div>
+                @else
+                <div class="avatar-profile">
+                    <img src="{{ asset('img/default-avatar.jpg')}}" />
+                </div>
+                @endif
                 <div class="data-user">
-                    <h2 class="username-profile">{{$user->username}}</h2>
-                    <h2 class="name-surname-profile">{{$user->name.' '.$user->surname}}</h2>
+                    <h5 class="username-profile">{{$user->username}}</h5>
+                    <h5 class="name-surname-profile">{{$user->name.' '.$user->surname}}</h5>
+                </div>
+                <div class="container description-profile-box">
                     <p class="description-profile">{{$user->description}}</p>
                 </div>
+                <div class="clearfix"></div>
+                <hr>
             </div>
 
+            @if(count($user->images) == 0)
+                <div class="profile-empty">
+                    <center><h3>No Posts Yet</h3></center>
+                </div>
+            @else
             <!--Bucle de publicaciones-->
-            @foreach($user->images as $image)
-            @include('includes.image', ['image'=>$image])
-            @endforeach
+                @foreach($user->images as $image)
+                    @include('includes.image', ['image'=>$image])
+                @endforeach
+            @endif
 
         </div>
     </div>
