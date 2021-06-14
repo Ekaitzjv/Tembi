96450bd53232b6b761961e9be3fc1945784283ba
Ekaitz Jimenez <ekaitzj@gmail.com>
2021-06-15 01:37:12 +0200

    Anti copy added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,157 +23,155 @@
     <link rel="shortcut icon" href="{{ asset('img/tembi-mini.png') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!--Anti copy-->
+    <body ondragstart="return false" onselectstart="return false" oncontextmenu="return false">
+
+    <!--Scroll bar-->
+    
+    
+    
 </head>
 
-<!-- Cookie Consent by https://www.CookieConsent.com -->
-<script type="text/javascript" src="//www.cookieconsent.com/releases/4.0.0/cookie-consent.js" charset="UTF-8"></script>
-<script type="text/javascript" charset="UTF-8">
-document.addEventListener('DOMContentLoaded', function() {
-    cookieconsent.run({
-        "notice_banner_type": "interstitial",
-        "consent_type": "express",
-        "palette": "light",
-        "language": "en",
-        "page_load_consent_levels": ["strictly-necessary"],
-        "notice_banner_reject_button_hide": false,
-        "preferences_center_close_button_hide": false,
-        "website_name": "www.tembi.com.devel/",
-        "website_privacy_policy_url": "www.tembi.com.devel/privacy"
+    <!-- Cookie Consent by https://www.CookieConsent.com -->
+    <script type="text/javascript" src="//www.cookieconsent.com/releases/4.0.0/cookie-consent.js" charset="UTF-8">
+    </script>
+    <script type="text/javascript" charset="UTF-8">
+    document.addEventListener('DOMContentLoaded', function() {
+        cookieconsent.run({
+            "notice_banner_type": "interstitial",
+            "consent_type": "express",
+            "palette": "light",
+            "language": "en",
+            "page_load_consent_levels": ["strictly-necessary"],
+            "notice_banner_reject_button_hide": false,
+            "preferences_center_close_button_hide": false,
+            "website_name": "www.tembi.com.devel/",
+            "website_privacy_policy_url": "www.tembi.com.devel/privacy"
+        });
     });
-});
-</script>
-<!-- End Cookie Consent by https://www.CookieConsent.com -->
+    </script>
+    <!-- End Cookie Consent by https://www.CookieConsent.com -->
+
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <div class="logo">
+                            <img src="{{ asset('img/tembi.png')}}">
+                        </div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            <!--como invitado-->
+                            @guest
+                            <!--comprobar la ruta para saber que boton mostrar-->
+                            @if (Request::is('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                            @elseif (Request::is('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Log In') }}</a>
+                            </li>
+                            @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Log In') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+
+                            @endif
 
 
-<!-- Google Analytics -->
-<script type="text/plain" cookie-consent="tracking">
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', GOOGLE_PROPERTY_ID_GOES_HERE, 'auto');
-ga('send', 'pageview');
-</script>
-
-
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div class="logo">
-                        <img src="{{ asset('img/tembi.png')}}">
-                    </div>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        <!--como invitado-->
-                        @guest
-                        <!--comprobar la ruta para saber que boton mostrar-->
-                        @if (Request::is('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @elseif (Request::is('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Log In') }}</a>
-                        </li>
-                        @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Log In') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-
-                        @endif
-
-
-                        @else
-                        <li class="nav-item home-icon">
-                            <a class="nav-link top-icon" href="{{ route('home') }}">
-                                <img src="{{asset('img/home.png')}}" />
-                            </a>
-                        </li>
-                        <li class="nav-item people-icon">
-                            <a class="nav-link top-icon" href="{{ route('user.index') }}">
-                                <img src="{{asset('img/people.png')}}" />
-                            </a>
-                        </li>
-                        <li class="nav-item heart-icon">
-                            <a class="nav-link top-icon" href="{{ route('likes') }}">
-                                <img src="{{asset('img/Main-heart.png')}}" />
-                            </a>
-                        </li>
-                        <li class="nav-item add-icon">
-                            <a class="nav-link top-icon" href="{{ route('image.create') }}">
-                                <img src="{{asset('img/add.png')}}" />
-                            </a>
-                        </li>
-                        <a href="{{ route('profile', ['id' => Auth::user()->id]) }}">
-                            @include('includes.avatar')
-                        </a>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle name-dropdown top-icon" href="#"
-                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item top-dropdown"
-                                    href="{{ route('profile', ['id' => Auth::user()->id])}}">
-                                    My Profile
+                            @else
+                            <li class="nav-item home-icon">
+                                <a class="nav-link top-icon" href="{{ route('home') }}">
+                                    <img src="{{asset('img/home.png')}}" />
                                 </a>
-                                <a class="dropdown-item top-dropdown" href="{{ route('settings') }}">
-                                    Settings and privacy
+                            </li>
+                            <li class="nav-item people-icon">
+                                <a class="nav-link top-icon" href="{{ route('user.index') }}">
+                                    <img src="{{asset('img/people.png')}}" />
                                 </a>
-                                <a class="dropdown-item cerrar-sesion" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            </li>
+                            <li class="nav-item heart-icon">
+                                <a class="nav-link top-icon" href="{{ route('likes') }}">
+                                    <img src="{{asset('img/Main-heart.png')}}" />
+                                </a>
+                            </li>
+                            <li class="nav-item add-icon">
+                                <a class="nav-link top-icon" href="{{ route('image.create') }}">
+                                    <img src="{{asset('img/add.png')}}" />
+                                </a>
+                            </li>
+                            <a href="{{ route('profile', ['id' => Auth::user()->id]) }}">
+                                @include('includes.avatar')
+                            </a>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle name-dropdown top-icon" href="#"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                                    v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item top-dropdown"
+                                        href="{{ route('profile', ['id' => Auth::user()->id])}}">
+                                        My Profile
+                                    </a>
+                                    <a class="dropdown-item top-dropdown" href="{{ route('settings') }}">
+                                        Settings and privacy
+                                    </a>
+                                    <a class="dropdown-item cerrar-sesion" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            <li class="nav-item add-icon">
+                                <a class="nav-link top-icon top-bell" href="{{ route('activity') }}">
+                                    <img src="{{asset('img/bell.png')}}" />
                                 </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        <li class="nav-item add-icon">
-                            <a class="nav-link top-icon top-bell" href="{{ route('activity') }}">
-                                <img src="{{asset('img/bell.png')}}" />
-                            </a>
-                        </li>
-                        <li class="nav-item add-icon">
-                            <a class="nav-link top-icon top-trendy" href="{{ route('trendy') }}">
-                                <img src="{{asset('img/popular.png')}}" />
-                            </a>
-                        </li>
-                        @if (Auth::user()->role == '1234')
-                        <li class="nav-item add-icon">
-                            <a class="nav-link top-icon top-admin" href="{{ route('admin') }}">
-                                <img src="{{asset('img/admin.png')}}" />
-                            </a>
-                        </li>
-                        @endif
-                        @endguest
-                    </ul>
+                            </li>
+                            <li class="nav-item add-icon">
+                                <a class="nav-link top-icon top-trendy" href="{{ route('trendy') }}">
+                                    <img src="{{asset('img/popular.png')}}" />
+                                </a>
+                            </li>
+                            @if (Auth::user()->role == '1234')
+                            <li class="nav-item add-icon">
+                                <a class="nav-link top-icon top-admin" href="{{ route('admin') }}">
+                                    <img src="{{asset('img/admin.png')}}" />
+                                </a>
+                            </li>
+                            @endif
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+            </nav>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
 
 </html>
